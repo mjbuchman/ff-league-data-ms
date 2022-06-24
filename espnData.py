c8deba9f48916835a100c08efc9208ddc6fca68b
mjbuchman <mbookie213@gmail.com>
2022-06-24 15:06:06 -0400

    attempt 2

--- espnData.py
+++ espnData.py
@@ -11,7 +11,6 @@ def getMatchupData(startWeek, endWeek, year):
 	matchups = []
 	league = League(league_id=LEAGUE, year=year, espn_s2=ESPN_S2, swid=SWID)	
 	for week in range(startWeek,endWeek):
-		matchups.append({week: week})
 		for matchup in league.scoreboard(week):
 			matchups[week].append({	
       		"homeTeam": matchup.home_team.owner,
